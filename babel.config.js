812c1e2f7142b0405cb6a0e67245060c0df8132b
Ash <ashley@juice49.com>
2019-11-08 17:15:05 +0000

    Base setup with Parcel, React, and HMR

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  {
+    presets: ['env', 'react'],
+    plugins: ['react-hot-loader/babel']
+  }
+}
---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const App = () => (
+  <p>the ringing in my ears is from another life</p>
+)
+
+export default hot(module)(App)
