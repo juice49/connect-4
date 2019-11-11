b484d779dfa3f3b4138efff71c859f6814faab50
Ash <ashley@juice49.com>
2019-11-11 00:23:40 +0000

    Update demo state

--- src/components/app.js
+++ src/components/app.js
@@ -6,12 +6,12 @@ const gridSize = [7,6]
 const winningLineLength = 4
 
 const state = [
-  1,    0   , 1   , 1,    null,    1,    0,
-  0,    0   , null, null, 1,    null, null,
-  1,    1   , null, 1,    null, null, null,
-  1,    0   , 1   , null, 0,    null, null,
-  1,    null, null, null, null, null, null,
-  1,    null, null, null, null, null, null
+  null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null,
+  null, 1   , null, null, null, null, null,
+  null, 0   , 1   , null, null, null, null,
+  null, 0   , 0   , 1   , null, null, null,
+  1   , 1   , 0   , 0   , 1   , 0   , null
 ]
 
 console.log(findWin(state, gridSize, winningLineLength))
