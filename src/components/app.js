753cc16a30e92df0bbadfdf2f256a6fad8720453
Ash <ashley@juice49.com>
2019-11-16 23:46:22 +0000

    Add player names, display next player and winning player

--- src/components/app.js
+++ src/components/app.js
@@ -9,19 +9,33 @@ const gridSize = [7,6]
 const winningLineLength = 4
 
 const App = () => {
-  const { cells, placeCounter, winningLine } = useConnect4(gridSize)
+  const {
+    cells,
+    placeCounter,
+    nextValue,
+    winningValue,
+    winningLine
+  } = useConnect4(gridSize)
+
+  const [playerNames] = useState(['Player 1', 'Player 2'])
 
   return (
     <>
       <Board gridSize={gridSize}>
         {cells.map((value, index) => (
           <Cell key={index} onClick={() => placeCounter(index)}>
-            {value}
+            {(value !== null) && value + 1}
           </Cell>
         ))}
       </Board>
       {winningLine && (
-        <p>Winner: {JSON.stringify(winningLine)}</p>
+        <>
+          <p>Winner: {playerNames[winningValue]}</p>
+          <p>Line: {JSON.stringify(winningLine)}</p>
+        </>
+      )}
+      {!winningLine && (
+        <p>Current turn: {playerNames[nextValue]}</p>
       )}
     </>
   )
