94ed528603be2064e4b5363950157662c6172599
Ash <ashley@juice49.com>
2019-12-05 21:58:12 +0000

    Add some types!

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react'
-import findWin from '../lib/find-win'
+import GridSize from '../types/grid-size'
 import useConnect4 from '../lib/use-connect-4'
 import Board from './board'
 import Cell from './cell'
 
-const gridSize = [7,6]
-const winningLineLength = 4
+interface AppProps {
+  gridSize?: GridSize,
+  winningLineLength?: number
+}
 
-const App = () => {
+const App: React.FC<AppProps> = ({
+  gridSize = [7, 6],
+  winningLineLength = 4
+}) => {
   const {
     cells,
     placeCounter,
     nextValue,
     winningValue,
     winningLine
-  } = useConnect4(gridSize)
+  } = useConnect4(gridSize, winningLineLength)
 
   const [playerNames] = useState(['Player 1', 'Player 2'])
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from '../components/app'
+
+const Page: React.FC = () => (
+  <App
+    gridSize={[7, 6]}
+    winningLineLength={4}
+  />
+)
+
+export default Page
