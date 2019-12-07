8f4bc62ca91afe9d836cacd7be8fb6d10415bd13
Ash <ashley@juice49.com>
2019-12-07 22:00:19 +0000

    Convert `getNextCellInColumn` to TS

--- src/lib/get-next-cell-in-column.ts
+++ src/lib/get-next-cell-in-column.ts
@@ -1,13 +1,19 @@
+import GridSize from '../types/grid-size'
+import CellValue from '../types/cell-value'
 import getColumnCells from './get-column-cells'
 
-export default function getNextCellInColumn (gridSize, column, cells) {
+export default function getNextCellInColumn (
+  gridSize: GridSize,
+  column: number,
+  cells: CellValue[]
+): number | null {
   let previousCell = null
 
   if (cells[column] !== null) {
     return null
   }
 
-  for (const cell of getColumnCells(gridSize, column, cells)) {
+  for (const cell of getColumnCells(gridSize, column)) {
     const cellValue = cells[cell]
 
     if (cellValue !== null && previousCell !== null) {
